868040760a3b8dc3b92d93641f36fd12093b68f1
DevWasi <wasibiit@gmail.com>
2019-03-19 07:03:17 +0500

    1st Attempt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
-// import Grid from "@material-ui/core/Grid";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
 import TextField from "@material-ui/core/TextField";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'outcast',
+      username: '',
       realName: '',
       avatar: '',
       location: '',
@@ -18,36 +19,54 @@ class App extends Component {
       notFound: ''
     };
   }
-  getUser(username) {
-    let url = `https://api.github.com/users/${username}`;
-    fetch(url)
-      .then((res) => res.json() )
+
+  handleSubmit = () => {
+    fetch(`https://api.github.com/users/${this.state.username}`)
+      .then((res) => res.json())
       .then((data) => {
         this.setState({
           username: data.login,
-        })
+          realName: data.name,
+          avatar: data.avatar_url,
+          location: data.location,
+          repos: data.public_repos,
+          followers: data.followers,
+          url: data.html_url,
+          notFound: data.message
+       })
       });
   }
-  componentDidMount() {
-    this.getUser(this.state.username);
-  }
 
-  async handleSubmit(e) {
-    e.preventDefault();
-    let user = await this.getUser(this.refs.usernmae.value);
-    this.setState({ username: user.login });
-  }
+  handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      this.handleSubmit()
+      e.preventDefault();
+    }
+  };
+
   render() {
     return (
       <div>
         <NavBar />
-        <form onSubmit={e => this.handleSubmit(e)}>
-          <TextField
-            ref="username"
-            className="TextField"
-            type="search"
-          />
-        </form>
+        <TextField
+          id="standard-search"
+          label="Search user"
+          type="search"
+          margin="normal"
+          value={this.state.username}
+          onChange={e => this.setState({
+            username: e.target.value
+          })}
+          onKeyPress={this.handleKeyPress}
+        />
+        <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="flex-start"
+        >
+
+        </Grid>
       </div>
     );
   }
